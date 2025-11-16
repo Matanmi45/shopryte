45dea877067d33ee4bc10d1d1c9e3907e8bad442
Tobi <markinstobi@gmail.com>
2025-11-16 15:15:43 +0100

    auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,22 @@
+import "dotenv/config";
+import "express-async-errors";
+import "src/db";
 import express from "express";
+import authRouter from "routes/auth";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+app.use(express.static("src/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// API Routes
+app.use("/auth", authRouter);
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({ message: err.message });
+} as express.ErrorRequestHandler);
+
+app.listen(8000, () => {
+  console.log("The app is running on http://localhost:8000");
+});
